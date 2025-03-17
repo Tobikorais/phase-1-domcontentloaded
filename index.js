39206a6833dbc2a7ba55463006c68cfffa442c42
Andrew Tobiko <andrew.tobiko1@student.moringaschool.com>
2025-03-17 14:29:26 +0300

    Adding files

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const targetElement = document.getElementById('text'); // Assuming the element has an ID of 'text'
+    targetElement.textContent = 'This is really cool!';
+});
